e0b41e7c62dfb2d966129fe8e57d453609bab210
Jesse Spears <jesse@mailgunhq.com>
2015-01-16 16:56:58 -0800

    Fixed bugs.  Confirmed to work.

--- source/_static/insert-api-key.js
+++ source/_static/insert-api-key.js
@@ -1,14 +1,4 @@
-// Insert the logged-in user's API key and active domain
-api_key_replace = function (){
-	// Retrieve the logged-in user's information
-	var x=XmlHttpRequest();
-	x.open("get", "https://mailgun.com/sessions/current");
-	if (x.status != 200) {
-		return;
-	}
-	var user = JSON.parse(x.responseText);
-
-	// Crawl the DOM and replace the API key and domain
+replace_dom=function(var user) {
 	var e=document.getElementsByTagName("*");
 	for (var i=0; i < e.length; i++) {
 		var children = e[i].childNodes;
@@ -18,19 +8,29 @@ api_key_replace = function (){
 				children[j].nodeValue = children[j].nodeValue.replace(
 					"YOUR_API_KEY", user.api_key);
 			}
-/*			if (children[j].nodeType == 3 &&
-				children[j].nodeValue.indexOf("YOU@YOUR_DOMAIN_NAME") > -1) {
-				children[j].nodeValue = children[j].nodeValue.replace(
-					"YOU@YOUR_DOMAIN_NAME", user.email);
+	                if (children[j].nodeType == 3 &&
+                                children[j].nodeValue.indexOf("YOU@YOUR_DOMAIN_NAME") > -1) {
+                                children[j].nodeValue = children[j].nodeValue.replace(
+                                        "YOU@YOUR_DOMAIN_NAME", user.email);
 			}
-*/
 			if (children[j].nodeType == 3 &&
 				children[j].nodeValue.indexOf("YOUR_DOMAIN_NAME") > -1) {
 				children[j].nodeValue = children[j].nodeValue.replace(
 					"YOUR_DOMAIN_NAME", user.active_domain);
 			}
-
 		}
 	}
 }
-
+retrieve_and_replace=function (){
+	var x= new XMLHttpRequest();
+	x.open("get", "https://mailgun.com/sessions/current");
+	x.onload= function() {
+		if (x.status != 200) {
+			return false;
+		}
+		var user = JSON.parse(x.responseText);
+		replace_dom(user);
+	};
+	x.send();
+}
+retrieve_and_replace();
